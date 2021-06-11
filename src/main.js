812611d3ff3dd8051ae617c50e6942554c40239e
KLag-SF <k.lagopus.sf19@gmail.com>
2021-06-11 09:49:52 +0900

    Add Header&Footer

--- src/main.js
+++ src/main.js
@@ -4,9 +4,12 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
 
+require('bootstrap')
+
 Vue.config.productionTip = false
 
 new Vue({
+  el: '#app',
   router,
   vuetify,
   render: h => h(App)
